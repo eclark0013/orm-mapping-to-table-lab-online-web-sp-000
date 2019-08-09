9021073c04a819499cbf0f308748c08a2e01e558
Eric Clark <eclark0013@gmail.com>
2019-08-09 18:52:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,8 +12,13 @@ class Student
 
   def self.create_table
     sql = <<-SQL
-      hi
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
       SQL
+    DB[:conn].execute(sql)
   end
 
 
